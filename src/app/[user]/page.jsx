791fee1ffb6e85a5fea69045ef934efe93385044
Toriqul Islam <toriqulislam296@yahoo.com>
2025-02-21 11:39:50 +0600

    li

--- src/app/[user]/page.jsx
+++ src/app/[user]/page.jsx
@@ -174,21 +174,21 @@ const Page = () => {
 
               <ul className="grid grid-cols-4 w-[100%]  justify-center my-[20px] gap-3">
                 {profile[0]?.fbLink && (
-                  <li className="border-2 border-[#565656] p-3 rounded-full flex items-center justify-center">
+                  <li className=" flex items-center justify-center">
                     <a href={`${profile[0]?.fbLink}`}>
                     <SocialIcon network="facebook"  size={20} />
                     </a>
                   </li>
                 )}
                 {profile[0]?.insta && (
-                  <li className="border-2 border-[#565656] p-3 rounded-full flex items-center justify-center">
+                  <li className=" flex items-center justify-center">
                     <a href={`${profile[0]?.insta}`}>
                          <SocialIcon network="instagram"  />
                     </a>
                   </li>
                 )}
                 {profile[0]?.twitter && (
-                  <li className="border-2 border-[#565656] p-3 rounded-full flex items-center justify-center">
+                  <li className="l flex items-center justify-center">
                     
                     <a href={`${profile[0]?.twitter}`}>
                     <SocialIcon network="twitter"/>
@@ -197,7 +197,7 @@ const Page = () => {
                   </li>
                 )}
                 {profile[0]?.linkedin && (
-                  <li className="border-2 border-[#565656] p-3 rounded-full flex items-center justify-center">
+                  <li className=" flex items-center justify-center">
                    
                     <a href={`${profile[0]?.linkedin}`}>
                     <SocialIcon network="linkedin" />
